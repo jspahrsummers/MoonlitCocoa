99e1bd92accd595744c9c88abf781a545580d2a6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-10-24 13:36:07 -0700

    Expanded Metalua test to include pattern matching

--- Demos/MLC OS X Demo/MLC OS X Demo/hello.lua
+++ Demos/MLC OS X Demo/MLC OS X Demo/hello.lua
@@ -2,4 +2,12 @@
 
 require 'metalua.runtime'
 
-print 'Hello world!'
+function fib (n)
+	match n with
+	| 0 -> return 1
+	| 1 -> return 1
+	| n -> return fib(n - 1) + fib(n - 2)
+	end
+end
+
+print(fib(4))
