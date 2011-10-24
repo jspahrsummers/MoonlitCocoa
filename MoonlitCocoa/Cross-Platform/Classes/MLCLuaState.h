60288cc7d56a743f2356af77f9b856867b593e4b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-10-23 20:04:08 -0700

    Created an MLCLuaState class to represent a Lua state

---
+++ MoonlitCocoa/Cross-Platform/Classes/MLCLuaState.h
@@ -0,0 +1,35 @@
+//
+//  MLCLuaState.h
+//  MoonlitCocoa
+//
+//  Created by Justin Spahr-Summers on 23.10.11.
+//  Released into the public domain.
+//
+
+#import <Foundation/Foundation.h>
+#import <lua.h>
+
+/**
+ * Represents a Lua state, which is a thread in a Lua execution context.
+ * Multiple Lua state objects may share the same global state, if some are
+ * threads created from one original.
+ */
+@interface MLCLuaState : NSObject
+/**
+ * The Lua state object owned by the receiver.
+ */
+@property (nonatomic, readonly) lua_State *state;
+
+/**
+ * Initializes the receiver as a new Lua state, using the default allocator.
+ */
+- (id)init;
+
+/**
+ * Initializes the receiver with the given Lua state object. If \a closeWhenDone
+ * is \c YES, the state object is closed when the receiver is deallocated.
+ *
+ * This is the designated initializer for this class.
+ */
+- (id)initWithState:(lua_State *)state closeWhenDone:(BOOL)closeWhenDone;
+@end
---
+++ MoonlitCocoa/Cross-Platform/Classes/MLCLuaState.m
@@ -0,0 +1,48 @@
+//
+//  MLCLuaState.m
+//  MoonlitCocoa
+//
+//  Created by Justin Spahr-Summers on 23.10.11.
+//  Released into the public domain.
+//
+
+#import "MLCLuaState.h"
+#import <lauxlib.h>
+
+@interface MLCLuaState ()
+@property (nonatomic, readwrite) lua_State *state;
+@property (nonatomic, assign) BOOL closeWhenDone;
+@end
+
+@implementation MLCLuaState
+@synthesize state = m_state;
+@synthesize closeWhenDone = m_closeWhenDone;
+
+- (id)init {
+	lua_State *state = luaL_newstate();
+	if (!state) {
+		return nil;
+	}
+
+	return [self initWithState:state closeWhenDone:YES];
+}
+
+- (id)initWithState:(lua_State *)state closeWhenDone:(BOOL)closeWhenDone; {
+  	NSParameterAssert(state != NULL);
+
+  	if ((self = [super init])) {
+		self.state = state;
+		self.closeWhenDone = closeWhenDone;
+	}
+
+	return self;
+}
+
+- (void)dealloc {
+  	if (self.closeWhenDone) {
+		lua_close(self.state);
+		self.state = NULL;
+	}
+}
+
+@end
