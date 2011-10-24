3f0890d9ddae8f1fa8db33e0382f8cd4b901c8b8
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-10-23 19:59:05 -0700

    Creating basic global framework headers

---
+++ MoonlitCocoa/Cross-Platform/MoonlitCocoa.h
@@ -0,0 +1,8 @@
+//
+//  MoonlitCocoa.h
+//  MoonlitCocoa
+//
+//  Created by Justin Spahr-Summers on 23.10.11.
+//  Released into the public domain.
+//
+
---
+++ MoonlitCocoa/MoonlitCocoaTouch/MoonlitCocoaTouch.h
@@ -0,0 +1,9 @@
+//
+//  MoonlitCocoaTouch.h
+//  MoonlitCocoa
+//
+//  Created by Justin Spahr-Summers on 23.10.11.
+//  Released into the public domain.
+//
+
+#import <MoonlitCocoa/MoonlitCocoa.h>
