77c9e15113df00e87d883f78f3705e5b8d583730
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-10-23 21:00:59 -0700

    Created <MLCLuaValue> protocol to represent a value compatible with Lua

--- MoonlitCocoa/Cross-Platform/MoonlitCocoa.h
+++ MoonlitCocoa/Cross-Platform/MoonlitCocoa.h
@@ -7,3 +7,4 @@
 //
 
 #import <MoonlitCocoa/MLCLuaState.h>
+#import <MoonlitCocoa/MLCLuaValue.h>
---
+++ MoonlitCocoa/Cross-Platform/Protocols/MLCLuaValue.h
@@ -0,0 +1,35 @@
+//
+//  MLCLuaValue.h
+//  MoonlitCocoa
+//
+//  Created by Justin Spahr-Summers on 23.10.11.
+//  Released into the public domain.
+//
+
+#import <Foundation/Foundation.h>
+
+@class MLCLuaState;
+
+/**
+ * An object that can be converted to and from a valid Lua value.
+ */
+@protocol MLCLuaValue <NSObject>
+@required
+/**
+ * Returns whether the value at \a index in the stack of the given Lua state is
+ * compatible with the receiver. If \c YES is returned, you can create an
+ * instance of the receiver from the same stack index.
+ */
++ (BOOL)isInStack:(MLCLuaState *)state atIndex:(int)index;
+
+/**
+ * Returns the value at \a index in the stack of the given Lua state, or \c nil
+ * if the value at the index cannot be converted to an instance of the receiver.
+ */
++ (id)valueFromStack:(MLCLuaState *)state atIndex:(int)index;
+
+/**
+ * Pushes the receiver onto the top of the stack of the given Lua state.
+ */
+- (void)pushOntoStack:(MLCLuaState *)state;
+@end
