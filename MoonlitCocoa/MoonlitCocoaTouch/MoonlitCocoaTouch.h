57a0b349893bb9f9e37d577c1c9fdbfa0917024a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-10-24 11:02:52 -0700

    Removing existing project and workspace, to start from scratch and keep it simple, damnit

--- MoonlitCocoa/MoonlitCocoaTouch/MoonlitCocoaTouch.h
+++
@@ -1,9 +0,0 @@
-//
-//  MoonlitCocoaTouch.h
-//  MoonlitCocoa
-//
-//  Created by Justin Spahr-Summers on 23.10.11.
-//  Released into the public domain.
-//
-
-#import <MoonlitCocoa/MoonlitCocoa.h>
