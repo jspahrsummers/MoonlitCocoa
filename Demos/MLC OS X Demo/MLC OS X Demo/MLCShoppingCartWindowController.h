61d1e5e6cbe130a4f1dff99a512ba3f01ad32795
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-10-25 16:09:07 -0700

    Stubbed out Shopping Cart window controller

---
+++ Demos/MLC OS X Demo/MLC OS X Demo/MLCShoppingCartWindowController.h
@@ -0,0 +1,13 @@
+//
+//  MLCShoppingCartWindowController.h
+//  MLC OS X Demo
+//
+//  Created by Justin Spahr-Summers on 25.10.11.
+//  Released into the public domain.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MLCShoppingCartWindowController : NSWindowController
+
+@end
---
+++ Demos/MLC OS X Demo/MLC OS X Demo/MLCShoppingCartWindowController.m
@@ -0,0 +1,30 @@
+//
+//  MLCShoppingCartWindowController.m
+//  MLC OS X Demo
+//
+//  Created by Justin Spahr-Summers on 25.10.11.
+//  Released into the public domain.
+//
+
+#import "MLCShoppingCartWindowController.h"
+
+@implementation MLCShoppingCartWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
