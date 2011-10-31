452f1d840d2cc47fc82447b78f96df89d058ddf6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-10-30 17:14:46 -0700

    Added @lua_interface macro to declare methods that will be implemented in Lua (so reflection can get their type signatures)

--- Framework/MoonlitCocoa/MoonlitCocoa/MLCModel.h
+++ Framework/MoonlitCocoa/MoonlitCocoa/MLCModel.h
@@ -9,6 +9,15 @@
 #import <Foundation/Foundation.h>
 
 /**
+ * Declares, in a protocol \a NAME, methods that will be implemented in Lua. An
+ * object bridged into Lua can then conform to protocol \a NAME to indicate its
+ * ability to invoke those Lua methods.
+ */
+#define lua_interface(NAME) \
+	protocol NAME <NSObject> \
+	@optional
+
+/**
  * An abstract class representing an immutable model object bridged into Lua.
  * This class can be subclassed to get standard model object behaviors and Lua
  * bridging with minimal boilerplate.
