ab9e2cee3b3485b1ead6804f6cd0d004f08fd47d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-11-04 15:08:56 -0700

    Moved lua_interface macro into MLCBridgedObject header, instead of MLCModel

--- Framework/MoonlitCocoa/MoonlitCocoa/MLCBridgedObject.h
+++ Framework/MoonlitCocoa/MoonlitCocoa/MLCBridgedObject.h
@@ -8,6 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
-@interface MLCBridgedObject : NSObject
+/**
+ * Declares, in a protocol \a NAME, methods that will be implemented in Lua. An
+ * object bridged into Lua can then conform to protocol \a NAME to indicate its
+ * ability to invoke those Lua methods.
+ */
+#define lua_interface(NAME) \
+	protocol NAME <NSObject> \
+	@optional
 
 @end
