7f6d48aed72ab73759f79cb5f8ff88e22d470bd6
Josh Vera <jvera@ubermind.com>
2011-11-06 21:56:02 -0800

    Return a valid NSNumber if the value on the stack IS a number (not only if it isn't)

--- Framework/MoonlitCocoa/MoonlitCocoa/NSNumber+LuaAdditions.m
+++ Framework/MoonlitCocoa/MoonlitCocoa/NSNumber+LuaAdditions.m
@@ -1,9 +1,9 @@
 //
-//  NSNumber+LuaAdditions.m
-//  MoonlitCocoa
+//	NSNumber+LuaAdditions.m
+//	MoonlitCocoa
 //
-//  Created by Justin Spahr-Summers on 30.10.11.
-//  Released into the public domain.
+//	Created by Justin Spahr-Summers on 30.10.11.
+//	Released into the public domain.
 //
 
 #import "NSNumber+LuaAdditions.h"
@@ -18,21 +18,21 @@
 	NSNumber *obj = nil;
 	if (lua_isboolean(state.state, -1)) {
 		obj = [NSNumber numberWithBool:(BOOL)lua_toboolean(state.state, -1)];
-	} else if (!lua_isnumber(state.state, -1)) {
+	} else if (lua_isnumber(state.state, -1)) {
 		double num = lua_tonumber(state.state, -1);
 		obj = [NSNumber numberWithDouble:num];
 	}
 
 	if (obj)
 		lua_pop(state.state, 1);
-	
+
 	return obj;
 }
 
 - (void)pushOntoStack:(MLCState *)state; {
 	[state growStackBySize:1];
 
-  	lua_pushnumber(state.state, [self doubleValue]);
+	lua_pushnumber(state.state, [self doubleValue]);
 }
 
 @end
